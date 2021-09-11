1e97d99f42bc9c99eecea7c7f6a72d0ab1842b4a
tarekzoka <65197356+tarekzoka@users.noreply.github.com>
2021-09-11 22:58:06 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -9,27 +9,8 @@
 # ###########################################
 
 
-###########################################
-MY_URL="https://raw.githubusercontent.com/tarekzoka/channel/main/"  
-echo " remove old channel "
-# Remove any Channel  # 
-rm -rf /etc/enigma2/lamedb 
-rm -rf /etc/enigma2/*list 
-rm -rf /etc/enigma2/*.tv rm -rf /etc/enigma2/*.radio 
-rm -rf /etc/tuxbox/*.xml 
-echo " download and install channel "
-wget -O /var/volatile/tmp/enigma2-tarek-channel_1.2_all "https://raw.githubusercontent.com/tarekzoka/channel/main/enigma2-tarek-channel_1.2_all"
+opkg install --force-overwrite  https://github.com/tarekzoka/channel/raw/main/enigma2-tarek-channel_1.2_all.ipk
 wait
-tar xzvpf /tmp/enigma2-tarek-channel_1.2_all  wait
 sleep 2;
-echo "" 
-echo "" 
-echo "" "*********************************************************" 
-echo "# Channel And INSTALLED SUCCESSFULLY #"
-echo "          UPLOADED BY tarekzoka "
-echo " "*********************************************************" 
-echo "# your Device will RESTART Now #" echo "****************************************"
-wait 2
-init 4
-init 3
 exit 0
+
