0de247b23998bd0fb0c98ae0d16ce93a605c5f43
tarekzoka <65197356+tarekzoka@users.noreply.github.com>
2021-09-11 22:59:43 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -6,9 +6,6 @@
 #
 # Command: wget https://raw.githubusercontent.com/tarekzoka/channel/main/installer.sh -qO - | /bin/sh 
 #
-# ###########################################
-
-
 opkg install --force-overwrite  https://github.com/tarekzoka/channel/raw/main/enigma2-tarek-channel_1.2_all.ipk
 wait
 sleep 2;
